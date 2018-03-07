dc9a645ef287fbd7060c4e9aea3726b4f304af7a
Joe Quince <jquince@gmail.com>
2018-03-06 22:00:25 -0800

    fix null cases and some other none happy path cases
    add some comments

--- frontend/src/app/service/string-stats.service.ts
+++ frontend/src/app/service/string-stats.service.ts
@@ -5,7 +5,7 @@ import {ConfigService} from "./config.service";
 @Injectable()
 export class StringStatsService {
 
-  PHRASE_REG = /[^;,.]+[;,.]/ig;
+  PHRASE_REG = /[^;,.]+[;,.]?/ig;//updated to include the remainder 'good; not as good before'
   constructor(
     private apiService: ApiService,
     private config: ConfigService
@@ -15,28 +15,49 @@ export class StringStatsService {
     return this.apiService.post(this.config.stats_url,input);
   }
 
+  /*
+  * Breaks down a string to reverse sentence order,
+   * Breaks sentences into phrases to be further reversed
+  * */
   reverse(input: string) {
-    let lines = input.split('\n');
-    let output = [];
-    lines.forEach((sentence)=>{
-      let phrases = [];
-      const matches = sentence.match(this.PHRASE_REG);
-      if(matches){
-        matches.forEach((phrase) => {
-          phrases.push(this.reversePhrase(phrase))
-        });
-      }
-      output.push(phrases.join(''));
-    });
-    return output.reverse().join('\n');
+    if (input) {//if not (null, undefined, or empty)
+      let lines = input.split('\n');
+      let output = [];
+      lines.forEach((sentence) => {
+        let phrases = [];
+        const matches = sentence.match(this.PHRASE_REG);
+        if (matches) {
+          matches.forEach((phrase) => {
+            phrases.push(this.reversePhrase(phrase))
+          });
+        } else {//no phrase!!! still might be a word or group of words in first line though
+          phrases.push(this.reversePhrase(sentence));
+        }
+        output.push(phrases.join(''));
+      });
+      return output.reverse().join('\n');
+    }
   }
 
+  /*
+  * Takes input as string that should end in ';', ',', or '.')
+  * */
   reversePhrase(input: string) {
-    const cutOff = input.length-1;
-    const delim = input[cutOff];
-    let theRest = input.substring(0,cutOff);
-    theRest = theRest.trim();
-    const leadSpace = cutOff - theRest.length;//I think the way it's set up we are only trimming from the left
-    return " ".repeat(leadSpace)+ theRest.split(' ').reverse().join(' ')+delim;
+    let reversedPhrase = "";
+    if (input) {//if not (null, undefined, or empty)
+      let cutOff = input.length - 1;//position of last character and length of string after pluck
+      let delim = "";
+      let theRest = input;
+      if (input[cutOff].match(/[;,.]/)) {//truthy or null
+        delim = input[cutOff];// pluck our delimiter
+        theRest = input.substring(0, cutOff);//drop delim from the string to be processed, to put it back in the right place later
+      } else {
+        cutOff = input.length; //no delim input not touched
+      }
+      theRest = theRest.trim();// leave spaces in the right place as well
+      const leadSpace = cutOff - theRest.length;//I think the way it's set up we are only trimming from the left
+      reversedPhrase = " ".repeat(leadSpace) + theRest.split(' ').reverse().join(' ') + delim;
+    }
+    return reversedPhrase;
   }
 }
--- server/src/main/java/com/bfwg/service/impl/StringStatsServiceImpl.java
+++ server/src/main/java/com/bfwg/service/impl/StringStatsServiceImpl.java
@@ -2,6 +2,7 @@ package com.bfwg.service.impl;
 
 import com.bfwg.service.StringStatsService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +11,11 @@ import java.util.regex.Pattern;
 
 @Service
 public class StringStatsServiceImpl implements StringStatsService {
+    /*
+    * Uses regex to count number of matches
+    * @param input string to be processed
+    * @return A map containing stats about the processed string.
+    * */
     @Override
     public Map<String, String> getStats(String input) {
         Map<String, String> stats = new HashMap();
@@ -26,9 +32,13 @@ public class StringStatsServiceImpl implements StringStatsService {
     Pattern SPACE = Pattern.compile(" ",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
 
 
+    /*
+    * get the number of matches
+    *
+    * */
     int countFromMatcher(Matcher m){
         int count = 0;
-        while(m.find()){
+        while(m.find()){//seems lame but matcher won't give you the count otherwise
             count++;
         }
         return count;
@@ -36,26 +46,26 @@ public class StringStatsServiceImpl implements StringStatsService {
 
     @Override
     public int countVowels(String input) {
-        return countFromMatcher(VOWELS.matcher(input));
+        return StringUtils.isEmpty(input) ? 0 : countFromMatcher(VOWELS.matcher(input));
     }
 
     @Override
     public int countWords(String input) {
-        return input.split("[\n\r ]").length;
+        return StringUtils.isEmpty(input) ? 0 : input.split("[\n\r ]").length;
     }
 
     @Override
     public int countLines(String input) {
-        return input.split("\n").length;
+        return StringUtils.isEmpty(input) ? 0 : input.split("\n").length;
     }
 
     @Override
     public int countConsonants(String input) {
-        return countFromMatcher(CONSONANTS.matcher(input));
+        return StringUtils.isEmpty(input) ? 0 : countFromMatcher(CONSONANTS.matcher(input));
     }
 
     @Override
     public int countSpaces(String input) {
-        return countFromMatcher(SPACE.matcher(input));
+        return StringUtils.isEmpty(input) ? 0 : countFromMatcher(SPACE.matcher(input));
     }
 }
