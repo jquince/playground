68dfc0cb8dcd46b9eb92c0d88f9762fd1cfae1d0
Joe Quince <jquince@gmail.com>
2018-03-14 20:40:43 -0700

    Algo without regex
    added suggested test cases

--- server/src/main/java/com/bfwg/service/impl/StringStatsServiceImpl.java
+++ server/src/main/java/com/bfwg/service/impl/StringStatsServiceImpl.java
@@ -18,18 +18,62 @@ public class StringStatsServiceImpl implements StringStatsService {
     * */
     @Override
     public Map<String, String> getStats(String input) {
+        Long vowelsCount = new Long(0);
+        Long wordsCount = new Long(0);
+        Long linesCount = new Long(1);
+        Long consonantsCount = new Long(0);
+        Long spacesCount = new Long(0);
+        Boolean inWord = false;
+        Character curr = null;
+        String vowels = "aeiou";
+        String testString = input.toLowerCase();
+        if(!StringUtils.isEmpty(input)){
+            for (int i = 0; i < input.length(); i++) {
+                curr = testString.charAt(i);// get current char
+                if(curr >= 'a' && curr <= 'z') {//is it a word char
+                    if(!inWord){
+                        wordsCount++;//starting a word let's count it
+                    }
+                    if(vowels.indexOf(curr) >= 0){//vowel found
+                        vowelsCount++;
+                    } else {
+                        consonantsCount++;
+                    }
+                    inWord = true;
+                } else {
+                    if(curr == ' ') {//space
+                        spacesCount++;
+                    } else if(curr == '\n'){
+                        linesCount++;
+                    }
+                    //check for word boundary
+                    if (curr != '\'') {
+                        inWord = false;
+                    }
+                }
+            }
+        } else {
+            linesCount--;//no string set to zero
+        }
+
         Map<String, String> stats = new HashMap();
-        stats.put("vowels", String.valueOf(countVowels(input)));
+        stats.put("vowels", String.valueOf(vowelsCount));
+        stats.put("words", String.valueOf(wordsCount));
+        stats.put("lines", String.valueOf(linesCount));
+        stats.put("consonants", String.valueOf(consonantsCount));
+        stats.put("spaces", String.valueOf(spacesCount));
+        /*stats.put("vowels", String.valueOf(countVowels(input)));
         stats.put("words", String.valueOf(countWords(input)));
         stats.put("lines", String.valueOf(countLines(input)));
         stats.put("consonants", String.valueOf(countConsonants(input)));
-        stats.put("spaces", String.valueOf(countSpaces(input)));
+        stats.put("spaces", String.valueOf(countSpaces(input)));*/
         return stats;
     }
 
     Pattern VOWELS = Pattern.compile("[aeiou]",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
     Pattern CONSONANTS = Pattern.compile("[bcdfghjklmnpqrstvwxyz]",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
     Pattern SPACE = Pattern.compile(" ",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
+    Pattern WORDS = Pattern.compile("[^\r\n]\\w+('\\w\\w?)?",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
 
 
     /*
@@ -51,12 +95,12 @@ public class StringStatsServiceImpl implements StringStatsService {
 
     @Override
     public int countWords(String input) {
-        return StringUtils.isEmpty(input) ? 0 : input.split("[\n\r ]").length;
+        return StringUtils.isEmpty(input) ? 0 : countFromMatcher(WORDS.matcher(input));
     }
 
     @Override
     public int countLines(String input) {
-        return StringUtils.isEmpty(input) ? 0 : input.split("\n").length;
+        return StringUtils.isEmpty(input) ? 0 : input.split("\r\n?").length;
     }
 
     @Override
--- server/src/test/java/com/bfwg/service/StringStatsServiceTest.java
+++ server/src/test/java/com/bfwg/service/StringStatsServiceTest.java
@@ -3,6 +3,9 @@ package com.bfwg.service;
 import com.bfwg.AbstractTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
@@ -16,24 +19,52 @@ public class StringStatsServiceTest extends AbstractTest {
             "Mauris quis facilisis metus, vel vestibulum ante.";
 
     @Test
+    public void testGetStats() {
+        Map<String,String> results = stringStatsService.getStats(testString);
+        assertThat(results.get("words"),is("26"));
+        assertThat(results.get("lines"),is("3"));
+        assertThat(results.get("vowels"),is("65"));
+        assertThat(results.get("consonants"),is("84"));
+        assertThat(results.get("spaces"),is("23"));
+    }
+/*
+
+    @Test
     public void testVowelCount() {
         assertThat(stringStatsService.countVowels(testString),is(65));
     }
+*/
 
     @Test
     public void testWordCount() {
-        assertThat(stringStatsService.countWords(testString),is(26));
+        Map<String,String> results = stringStatsService.getStats(testString);
+        assertThat(results.get("words"),is("26"));
+        results = stringStatsService.getStats("hello,world");
+        assertThat("comma",results.get("words"),is("2"));
+        results = stringStatsService.getStats("hello\tworld");
+        assertThat("tab",results.get("words"),is("2"));
+        results = stringStatsService.getStats("hello    world");
+        assertThat("space",results.get("words"),is("2"));
+        results = stringStatsService.getStats("wait a sec\r\nhow many words\r\ndoes this have?");
+        assertThat("carriage",results.get("words"),is("9"));
+        results = stringStatsService.getStats("This exercise can be quite challenging; take your time.\n" +
+                "If you're not careful, it can really do you in. But don't worry.\n" +
+                "This fun challenge shows how you, the developer, solve problems.");
+        assertThat(results.get("words"),is("32"));
+
     }
-    @Test
+   /* @Test
     public void testLineCount() {
         assertThat(stringStatsService.countLines(testString),is(3));
+        assertThat(stringStatsService.countLines("wait a sec\\r\\nhow many words\\r\\ndoes this have?"),is(3));
     }
     @Test
     public void testConsonantCount() {
         assertThat(stringStatsService.countConsonants(testString),is(84));
-    }
+    }*/
     @Test
     public void testSpaceCount() {
-        assertThat(stringStatsService.countSpaces(testString),is(23));
+        Map<String,String> results = stringStatsService.getStats("      ");
+        assertThat(results.get("spaces"),is("6"));
     }
 }
